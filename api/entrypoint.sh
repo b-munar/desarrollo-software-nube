834ba53ae06b29e317b83318a8e98b1dacd5dc3f
b-munar <b.munar@uniandes.edu.co>
2023-04-14 10:34:54 -0500

    Implementacion de tareas periodicas con redis y celery

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -6,4 +6,8 @@ python3 -m flask db migrate
 
 python3 -m flask db upgrade
 
+mkdir -p /home/celery/var/run/
+
+celery -A tasks worker -B -s /home/celery/var/run/celerybeat-schedule --detach
+
 python -m flask run --host=0.0.0.0
--- api/tasks.py
+++ api/tasks.py
@@ -3,6 +3,14 @@ from config import Config
 
 app = Celery('tasks', backend=Config.CELERY_RESULT_BACKEND, broker=Config.CELERY_BROKER_URL )
 
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(5.0, queueing.s())
+
+@app.task
+def queueing(args):
+    print(args)
+
 @app.task
 def add(x, y):
     return x + y
