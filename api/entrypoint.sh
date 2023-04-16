6c08b02259ea526362baeb626f544e56c43c3bda
b-munar <b.munar@uniandes.edu.co>
2023-04-15 20:44:38 -0500

    Cambios en el entrypoint para favorecer el ubuntu server

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-python3 -m flask db init
+# python3 -m flask db init
 
-python3 -m flask db migrate
+# python3 -m flask db migrate
 
-python3 -m flask db upgrade
+# python3 -m flask db upgrade
 
-mkdir -p /home/celery/var/run/
+# mkdir -p /home/celery/var/run/
 
-celery -A tasks.compress.celery worker -B -s /home/celery/var/run/celerybeat-schedule --detach
+# celery -A tasks.compress.celery worker -B -s /home/celery/var/run/celerybeat-schedule --detach
 
-# python -m flask run --host=0.0.0.0
+# # python -m flask run --host=0.0.0.0
 
-gunicorn --bind 0.0.0.0:5000 wsgi:app
+# gunicorn --bind 0.0.0.0:5000 wsgi:app
+
+echo "La cabra"
