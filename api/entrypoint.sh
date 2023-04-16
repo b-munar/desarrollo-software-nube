1146e21dfab6be2b1511fdcbbfcdf3c958029f8a
b-munar <b.munar@uniandes.edu.co>
2023-04-15 22:49:30 -0500

    se agrego sleep al entrypoint

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+sleep 2
+
 python3 -m flask db init
+
+sleep 3
 
 python3 -m flask db migrate
 
+sleep 2
+
 python3 -m flask db upgrade
 
+sleep 2
+
 mkdir -p /home/celery/var/run/
+
+sleep 5
 
 celery -A tasks.compress.celery worker -B -s /home/celery/var/run/celerybeat-schedule --detach
 
