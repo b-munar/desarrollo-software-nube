b8f09ac6b254d64917c88234178e6c4b592eea6c
b-munar <b.munar@uniandes.edu.co>
2023-04-15 22:35:35 -0500

    se crearon dos docker, una para una maquina de minimos recursos, y otra para el minimo de los minimos

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# python3 -m flask db init
+python3 -m flask db init
 
-# python3 -m flask db migrate
+python3 -m flask db migrate
 
-# python3 -m flask db upgrade
+python3 -m flask db upgrade
 
-# mkdir -p /home/celery/var/run/
+mkdir -p /home/celery/var/run/
 
-# celery -A tasks.compress.celery worker -B -s /home/celery/var/run/celerybeat-schedule --detach
-
-# # python -m flask run --host=0.0.0.0
+celery -A tasks.compress.celery worker -B -s /home/celery/var/run/celerybeat-schedule --detach
 
 gunicorn --bind 0.0.0.0:5000 wsgi:app
---
+++ api/minimal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# python3 -m flask db init
+
+# python3 -m flask db migrate
+
+# python3 -m flask db upgrade
+
+# mkdir -p /home/celery/var/run/
+
+# celery -A tasks.compress.celery worker -B -s /home/celery/var/run/celerybeat-schedule --detach
+
+# # python -m flask run --host=0.0.0.0
+
+gunicorn --bind 0.0.0.0:5000 wsgi:app
