ab589f145309372031b284a9b55159daf346e5d1
b-munar <b.munar@uniandes.edu.co>
2023-04-16 19:39:02 -0500

    se optimizaron las funciones para comprimir

--- api/tasks/compress.py
+++ api/tasks/compress.py
@@ -1,4 +1,5 @@
 import zipfile
+import tarfile
 from models import File, Task
 from tasks.make_celery import make_celery
 from app import create_app
@@ -19,9 +20,32 @@ def compress_zip(file_id):
         task = Task.query.filter_by(file_id=file_id).first()
         task.status=True
         db.session.commit()
+        
+@celery.task()
+def compress_targz(file_id):
+    file_to_tar = File.query.filter(File.id == file_id).first() 
+    with tarfile.open(f"{file_to_tar.dir}/{file_to_tar.name.split('.')[0]}.tar.gz", 'w:gz') as tar:
+        tar.add(file_to_tar.path)
+        task = Task.query.filter_by(file_id=file_id).first()
+        task.status=True
+        db.session.commit()
+
+@celery.task()
+def compress_tarbz2(file_id):
+    file_to_tar = File.query.filter(File.id == file_id).first() 
+    with tarfile.open(f"{file_to_tar.dir}/{file_to_tar.name.split('.')[0]}.tar.bz2", 'w:bz2') as tar:
+        tar.add(file_to_tar.path)
+        task = Task.query.filter_by(file_id=file_id).first()
+        task.status=True
+        db.session.commit()
 
 @celery.task()
 def queueing():
-    for task_to_zip in Task.query.filter_by(status = False):
+    for task_to_zip in Task.query.filter_by(status = False, type_task=1):
         compress_zip.delay(task_to_zip.file_id)
+    for task_to_targz in Task.query.filter_by(status = False, type_task=2):
+        compress_targz.delay(task_to_targz.file_id)
+    for task_to_tarbz2 in Task.query.filter_by(status = False, type_task=3):
+        compress_tarbz2.delay(task_to_tarbz2.file_id)
+        
         
